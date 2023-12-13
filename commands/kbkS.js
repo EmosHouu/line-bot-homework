26907dcaa6d4586e6a7a1827e68127eea3ecb227
Emos Hou <emos2687@gmail.com>
2023-12-13 13:09:09 +0800

    first commit

---
+++ commands/kbkS.js
@@ -0,0 +1,76 @@
+// 這邊是做單品茶的回應 
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+import kbkTemplate from '../templates/kbk.js'
+import fs from 'node:fs'
+
+export default async (event) => {
+// export const a  = async(event) => {
+
+  try {
+    const { data } = await axios.get('https://www.kebuke.com/product/')
+    const $ = cheerio.load(data)
+    const replies = []
+    // 新的footer連結與按鈕文字
+    const newFooterUri = "https://www.kebuke.com/menu/"
+    const newFooterLabel = "嫌爛自己查"
+
+    
+    // 取出網站資料
+    $('.archive-product__products-item-img').slice(0, 4).each(function () {
+      
+      // 取出圖片和標題
+      const image = $(this).find('img').attr('src')
+      // const imageUrl = new URL(image, 'https://wdaweb.github.io/') 圖片不是網站連結時才要用
+
+      // 問老師為何不抓不到
+      // const title = $(this).find('.archive-product__products-item-name').text().trim()
+      const title2 = $(this).find('img').attr('src')
+      // 抓一串字裡的中文字
+      const chineseText = title2.match(/[\u4e00-\u9fa5]+/g).join('');
+      console.log(chineseText);
+
+      // 
+
+      // 產生一個新回應訊息模板
+      const template = kbkTemplate()
+      // 修改模板內容
+      template.hero.url = image 
+  
+      template.body.contents[0].text = chineseText
+
+      // console.log(title)
+   
+      // 更新點擊按鈕後連結的網站
+      // template.footer.contents.action.uri = KbkMenu 
+      // template.footer.contents[0].action[2] = "https://www.kebuke.com/menu/"
+      
+      // 修改模板中footer網站和按鈕
+      template.footer.contents[0].action.uri = newFooterUri
+      template.footer.contents[0].action.label = newFooterLabel
+
+      // 傳到模板
+      replies.push(template)
+
+      if(process.env.DBUG === 'true'){
+        fs.writeFileSync('./dump/kbk.json',JSON.stringify(template,null,2))
+      }
+    })
+
+
+    //回傳訊息
+    const result = await event.reply({
+      type: 'flex',
+      altText: '可不可',
+      contents: {
+        type: 'carousel',
+        contents: replies
+      }
+    })
+    console.log(result)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
---
+++ index.js
@@ -0,0 +1,112 @@
+// 機器人基本架構
+import 'dotenv/config'
+import linebot from 'linebot'
+// ----------------------------
+import kbk from './commands/kbk.js'
+import kbkR from './commands/kbkR.js'
+import kbkNg from './commands/kbkNg.js'
+import kbkMix from './commands/kbkMix.js'
+import kbkS from './commands/kbkS.js'
+
+
+
+
+
+
+// 機器人基本架構
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+// ----------------------------------------------
+
+bot.on('message', event => {
+  if (process.env.DEBUG === 'true') {
+    console.log(event)
+  }
+
+  if (event.message.type === 'text') {
+    if (event.message.text === '可不可') {
+      // kbk(event)
+      event.reply({
+        type:'text',
+        text:'可不可以幫我加薪，老鐵想喝啥呢？',
+        quickReply:{
+          items:[
+            {
+              type:'action',
+              action:{
+                //傳訊習
+                type:'message',
+                // 傳送的文字
+                text:'鮮奶茶',
+                // 按鈕的文字
+                label:'鮮奶茶'
+              }
+            },
+            {
+              type:'action',
+              action:{
+                //傳訊習
+                type:'message',
+                // 傳送的文字
+                text:'單品茶',
+                // 按鈕的文字
+                label:'單品茶'
+              }
+            },
+            {
+              type:'action',
+              action:{
+                //傳訊習
+                type:'message',
+                // 傳送的文字
+                text:'奶蓋',
+                // 按鈕的文字
+                label:'奶蓋'
+              }
+            },
+            {
+              type:'action',
+              action:{
+                //傳訊習
+                type:'message',
+                // 傳送的文字
+                text:'特調',
+                // 按鈕的文字
+                label:'特調'
+              }
+            },
+            {
+              type:'action',
+              action:{
+                //傳訊習
+                type:'message',
+                // 傳送的文字
+                text:'季節限定',
+                // 按鈕的文字
+                label:'季節限定'
+              }
+            }
+            
+          ]
+        }
+      }) 
+    }else if(event.message.text === '鮮奶茶'){
+      kbk(event)
+    }else if(event.message.text === '單品茶'|| event.message.text === '紅茶'|| event.message.text === '冬瓜茶'){
+      kbkR(event)
+    }else if(event.message.text === '奶蓋'){
+      kbkNg(event)
+    }else if(event.message.text === '特調'){
+      kbkMix(event)
+    }else if(event.message.text === '季節限定'){
+      kbkS(event)
+    }
+  }
+})
+// 機器人基本架構
+bot.listen('/', process.env.PORT || 2000, () => {
+  console.log('機器人啟動')
+})
